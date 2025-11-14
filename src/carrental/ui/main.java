717dee258a1faf32db22535e01e307eb4079aab2
LengxiQwQ <date200325@qq.com>
2025-11-14 13:12:11 +0800

    Initialization

--- src/carrental/ui/main.java
+++ src/carrental/ui/main.java
@@ -3,7 +3,7 @@ package carrental.ui;
 import java.net.SocketOption;
 
 public class main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         System.out.print("Hello World!");
     }
 }
